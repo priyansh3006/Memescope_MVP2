bf8ae8634be24dde233126e70b7f72a0bbbba691
priyansh3006 <priyansh3006@gmail.com>
2025-02-09 18:33:48 +0530

    Realtime data fetching is working now and pulumi,vpc part is complete too

--- pulumi-go-infra/main.go
+++ pulumi-go-infra/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/apigatewayv2"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/cloudwatch"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/dynamodb"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ecs"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/lambda"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/rds"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/sns"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ssm"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		//Created an VPC
+		//Starting the VPC service
 		vpc, err := ec2.NewVpc(ctx, "memescopeVpc", &ec2.VpcArgs{
 			CidrBlock:          pulumi.String("10.0.0.0/16"),
 			EnableDnsSupport:   pulumi.Bool(true),
@@ -19,7 +28,15 @@ func main() {
 			return err
 		}
 
-		//Created Two Public Subnets in Different Availability Zones
+		// Starting the Internet Gateway
+		igw, err := ec2.NewInternetGateway(ctx, "memescopeIgw", &ec2.InternetGatewayArgs{
+			VpcId: vpc.ID(),
+		})
+		if err != nil {
+			return err
+		}
+
+		// Starting the subnet1
 		subnet1, err := ec2.NewSubnet(ctx, "memescopeSubnet1", &ec2.SubnetArgs{
 			VpcId:               vpc.ID(),
 			CidrBlock:           pulumi.String("10.0.1.0/24"),
@@ -30,6 +47,7 @@ func main() {
 			return err
 		}
 
+		//Starting the subnet2
 		subnet2, err := ec2.NewSubnet(ctx, "memescopeSubnet2", &ec2.SubnetArgs{
 			VpcId:               vpc.ID(),
 			CidrBlock:           pulumi.String("10.0.2.0/24"),
@@ -39,15 +57,33 @@ func main() {
 		if err != nil {
 			return err
 		}
+		//Including the Internet Gateway
 
-		// Internet Gateway & Route Table
-		igw, err := ec2.NewInternetGateway(ctx, "memescopeIgw", &ec2.InternetGatewayArgs{
+		// Intializing the Security Group
+		securityGroup, err := ec2.NewSecurityGroup(ctx, "memescopeSG", &ec2.SecurityGroupArgs{
 			VpcId: vpc.ID(),
+			Ingress: ec2.SecurityGroupIngressArray{
+				&ec2.SecurityGroupIngressArgs{
+					Protocol:   pulumi.String("tcp"),
+					FromPort:   pulumi.Int(8080),
+					ToPort:     pulumi.Int(8080),
+					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+				},
+			},
 		})
 		if err != nil {
 			return err
 		}
 
+		// Stating the  RDS PostgreSQL Database
+		dbSubnetGroup, err := rds.NewSubnetGroup(ctx, "memescope-db-subnetgroup", &rds.SubnetGroupArgs{
+			SubnetIds: pulumi.StringArray{subnet1.ID(), subnet2.ID()},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Route Table & Route Associations
 		routeTable, err := ec2.NewRouteTable(ctx, "memescopeRouteTable", &ec2.RouteTableArgs{
 			VpcId: vpc.ID(),
 			Routes: ec2.RouteTableRouteArray{
@@ -61,7 +97,6 @@ func main() {
 			return err
 		}
 
-		// Associated Route Table with Subnets
 		_, err = ec2.NewRouteTableAssociation(ctx, "memescopeRouteTableAssoc1", &ec2.RouteTableAssociationArgs{
 			SubnetId:     subnet1.ID(),
 			RouteTableId: routeTable.ID(),
@@ -78,39 +113,41 @@ func main() {
 			return err
 		}
 
-		// Security Group for API and Database
-		securityGroup, err := ec2.NewSecurityGroup(ctx, "memescopeSG", &ec2.SecurityGroupArgs{
-			VpcId: vpc.ID(),
-			Ingress: ec2.SecurityGroupIngressArray{
-				&ec2.SecurityGroupIngressArgs{
-					Protocol:   pulumi.String("tcp"),
-					FromPort:   pulumi.Int(4000), // API Port
-					ToPort:     pulumi.Int(4000),
-					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
-				},
-				&ec2.SecurityGroupIngressArgs{
-					Protocol:   pulumi.String("tcp"),
-					FromPort:   pulumi.Int(5432), // PostgreSQL Port
-					ToPort:     pulumi.Int(5432),
-					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
-				},
-			},
-			Egress: ec2.SecurityGroupEgressArray{
-				&ec2.SecurityGroupEgressArgs{
-					Protocol:   pulumi.String("-1"),
-					FromPort:   pulumi.Int(0),
-					ToPort:     pulumi.Int(0),
-					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
-				},
-			},
+		// Starting the  ECS Cluster
+		ecsCluster, err := ecs.NewCluster(ctx, "memescopeCluster", &ecs.ClusterArgs{})
+		if err != nil {
+			return err
+		}
+
+		// ECS Task Definition
+		taskDefinition, err := ecs.NewTaskDefinition(ctx, "realTimeTaskDef", &ecs.TaskDefinitionArgs{
+			Family:      pulumi.String("real-time-service"),
+			Cpu:         pulumi.String("512"),
+			Memory:      pulumi.String("1024"),
+			NetworkMode: pulumi.String("awsvpc"),
+			ContainerDefinitions: pulumi.String(fmt.Sprintf(`[{
+				"name": "realtime-service",
+				"image": "your-dockerhub-username/realtime-service:latest",
+				"cpu": 512,
+				"memory": 1024,
+				"portMappings": [{"containerPort": 8080, "hostPort": 8080}]
+			}]`)),
 		})
 		if err != nil {
 			return err
 		}
 
-		// RDS PostgreSQL Database
-		dbSubnetGroup, err := rds.NewSubnetGroup(ctx, "memescope-db-subnetgroup", &rds.SubnetGroupArgs{
-			SubnetIds: pulumi.StringArray{subnet1.ID(), subnet2.ID()},
+		// Staring the ECS Service
+		service, err := ecs.NewService(ctx, "realTimeService", &ecs.ServiceArgs{
+			Cluster:        ecsCluster.Arn,
+			TaskDefinition: taskDefinition.Arn,
+			DesiredCount:   pulumi.Int(1),
+			LaunchType:     pulumi.String("FARGATE"),
+			NetworkConfiguration: &ecs.ServiceNetworkConfigurationArgs{
+				AssignPublicIp: pulumi.Bool(true),
+				SecurityGroups: pulumi.StringArray{securityGroup.ID()},
+				Subnets:        pulumi.StringArray{subnet1.ID(), subnet2.ID()},
+			},
 		})
 		if err != nil {
 			return err
@@ -121,8 +158,8 @@ func main() {
 			InstanceClass:       pulumi.String("db.t3.micro"),
 			AllocatedStorage:    pulumi.Int(20),
 			DbName:              pulumi.String("memescope"),
-			Username:            pulumi.String("memescope_user"),
-			Password:            pulumi.String("securepassword123"),
+			Username:            pulumi.String("memescope_user"),    //might change later
+			Password:            pulumi.String("securepassword123"), //will change during the deployment
 			SkipFinalSnapshot:   pulumi.Bool(true),
 			DbSubnetGroupName:   dbSubnetGroup.Name,
 			VpcSecurityGroupIds: pulumi.StringArray{securityGroup.ID()},
@@ -132,7 +169,7 @@ func main() {
 			return err
 		}
 
-		//  CloudWatch Log Group
+		// CloudWatch Log Group
 		_, err = cloudwatch.NewLogGroup(ctx, "memescopeLogGroup", &cloudwatch.LogGroupArgs{
 			RetentionInDays: pulumi.Int(14),
 		})
@@ -140,10 +177,141 @@ func main() {
 			return err
 		}
 
+		// //✅ Keep the Existing ECS Cluster for Real-Time WebSocket Service
+		// ecsCluster, err := ecs.NewCluster(ctx, "memescopeCluster", &ecs.ClusterArgs{})
+		// if err != nil {
+		// 	return err
+		//}
+
+		// Intializing the  DynamoDB Table
+		tradeTable, err := dynamodb.NewTable(ctx, "TradesTable", &dynamodb.TableArgs{
+			Attributes: dynamodb.TableAttributeArray{
+				&dynamodb.TableAttributeArgs{
+					Name: pulumi.String("TradeID"),
+					Type: pulumi.String("S"),
+				},
+			},
+			BillingMode:    pulumi.String("PAY_PER_REQUEST"),
+			HashKey:        pulumi.String("TradeID"),
+			StreamEnabled:  pulumi.Bool(true),
+			StreamViewType: pulumi.String("NEW_IMAGE"),
+		})
+		if err != nil {
+			return err
+		}
+
+		// // New SNS Topic for Trade Updates
+		// tradeUpdatesTopic, err := sns.NewTopic(ctx, "TradeUpdatesTopic", &sns.TopicArgs{})
+		// if err != nil {
+		// 	return err
+		// }
+
+		// ✅ Create SNS Topic (Ensuring it Always Exists)
+		tradeUpdatesTopic, err := sns.NewTopic(ctx, "TradeUpdatesTopic", &sns.TopicArgs{
+			Name: pulumi.String("TradeUpdatesTopic"), // Explicitly setting a name
+		})
+		if err != nil {
+			return err
+		}
+
+		// ✅ Force creation of the SSM Parameter by setting `ReplaceOnChanges`
+		_, err = ssm.NewParameter(ctx, "snsTopicArn", &ssm.ParameterArgs{
+			Name:      pulumi.String("snsTopicArn"),
+			Type:      pulumi.String("String"),
+			Value:     tradeUpdatesTopic.Arn,
+			Overwrite: pulumi.Bool(true), // Ensures parameter is updated if it exists
+			Tier:      pulumi.String("Standard"),
+		})
+		if err != nil {
+			return err
+		}
+
+		//New IAM Role for Lambda
+		lambdaRole, err := iam.NewRole(ctx, "lambdaExecutionRole", &iam.RoleArgs{
+			AssumeRolePolicy: pulumi.String(`{
+				"Version": "2012-10-17",
+				"Statement": [{
+					"Effect": "Allow",
+					"Principal": {"Service": "lambda.amazonaws.com"},
+					"Action": "sts:AssumeRole"
+				}]
+			}`),
+		})
+		if err != nil {
+			return err
+		}
+
+		// Create IAM Role for SSM
+		ssmRole, err := iam.NewRole(ctx, "ec2SSMRole", &iam.RoleArgs{
+			AssumeRolePolicy: pulumi.String(`{
+        	"Version": "2012-10-17",
+        	"Statement": [{
+            	"Effect": "Allow",
+            	"Principal": {"Service": "ec2.amazonaws.com"},
+            	"Action": "sts:AssumeRole"
+        }]
+    }`),
+		})
+		if err != nil {
+			return err
+		}
+
+		// Attach SSM Permissions
+		_, err = iam.NewRolePolicyAttachment(ctx, "ssmCoreAttachment", &iam.RolePolicyAttachmentArgs{
+			Role:      ssmRole.Name,
+			PolicyArn: pulumi.String("arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"),
+		})
+		if err != nil {
+			return err
+		}
+
+		// Attach IAM Role to EC2
+		_, err = ec2.NewInstance(ctx, "bastionHost", &ec2.InstanceArgs{
+			InstanceType: pulumi.String("t3.micro"),
+			Ami:          pulumi.String("ami-0c55b159cbfafe1f0"), // Replace with a valid Amazon Linux AMI
+			SubnetId:     subnet1.ID(),
+			VpcSecurityGroupIds: pulumi.StringArray{
+				securityGroup.ID(),
+			},
+			IamInstanceProfile: ssmRole.Name,
+		})
+		if err != nil {
+			return err
+		}
+
+		//New Lambda Function for WebSocket
+		websocketLambda, err := lambda.NewFunction(ctx, "WebSocketHandler", &lambda.FunctionArgs{
+			Runtime: pulumi.String("provided.al2"),
+			Handler: pulumi.String("main"),
+			Role:    lambdaRole.Arn,
+			Code:    pulumi.NewFileArchive("./realtime_service.zip"),
+		})
+		if err != nil {
+			return err
+		}
+
+		// New API Gateway WebSocket API
+		apiGateway, err := apigatewayv2.NewApi(ctx, "TradeWebSocketAPI", &apigatewayv2.ApiArgs{
+			ProtocolType:             pulumi.String("WEBSOCKET"),
+			RouteSelectionExpression: pulumi.String("$request.body.action"),
+		})
+		if err != nil {
+			return err
+		}
+
 		// Exporting the  Outputs
 		ctx.Export("vpcId", vpc.ID())
-		ctx.Export("dbEndpoint", db.Endpoint)
+		ctx.Export("subnet1Id", subnet1.ID())
+		ctx.Export("subnet2Id", subnet2.ID())
 		ctx.Export("securityGroupId", securityGroup.ID())
+		ctx.Export("dynamoDbTableName", tradeTable.Name)
+		ctx.Export("snsTopicArn", tradeUpdatesTopic.Arn)
+		ctx.Export("websocketLambdaArn", websocketLambda.Arn)
+		ctx.Export("apiGatewayUrl", apiGateway.ApiEndpoint)
+		ctx.Export("dbEndpoint", db.Endpoint) //  Useing the RDS instanc
+		ctx.Export("ecsClusterArn", ecsCluster.Arn)
+		ctx.Export("realTimeServiceID", service.ID())
+		ctx.Export("realTimeServiceName", service.Name)
 
 		return nil
 	})
---
+++ realtime_service/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/sns"
+	"github.com/aws/aws-sdk-go/service/ssm" // AWS Systems Manager Parameter Store
+	"github.com/gorilla/websocket"
+)
+
+// Initialize AWS Session
+var awsSession = session.Must(session.NewSession(&aws.Config{
+	Region: aws.String("us-east-1"),
+}))
+
+// Fetch Pulumi-managed resources
+var dynamoDBTable = getPulumiParameter("dynamoDbTableName") // DynamoDB Table provisioned via Pulumi
+var snsTopicArn = getPulumiParameter("snsTopicArn")         // SNS Topic provisioned via Pulumi
+
+var dynamoDB = dynamodb.New(awsSession)
+var snsClient = sns.New(awsSession)
+
+// WebSocket Config
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var mutex = sync.Mutex{}
+
+// Trade Action Struct
+type TradeAction struct {
+	TradeID   string  `json:"tradeId"`
+	Timestamp string  `json:"timestamp"`
+	Price     float64 `json:"price"`
+	Volume    int     `json:"volume"`
+	Trader    string  `json:"trader"`
+	Action    string  `json:"action"`
+}
+
+// Function to fetch Pulumi resource outputs from AWS SSM Parameter Store
+func getPulumiParameter(paramName string) string {
+	ssmClient := ssm.New(awsSession)
+	param, err := ssmClient.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String(paramName),
+		WithDecryption: aws.Bool(true),
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to get parameter %s: %v", paramName, err)
+	}
+
+	return *param.Parameter.Value
+}
+
+// WebSocket Handler
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("WebSocket Upgrade Error:", err)
+		return
+	}
+	defer conn.Close()
+
+	mutex.Lock()
+	clients[conn] = true
+	mutex.Unlock()
+
+	log.Println("New WebSocket connection")
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Client disconnected")
+			mutex.Lock()
+			delete(clients, conn)
+			mutex.Unlock()
+			break
+		}
+
+		var trade TradeAction
+		if err := json.Unmarshal(msg, &trade); err != nil {
+			log.Println("Invalid trade action format:", err)
+			continue
+		}
+
+		trade.TradeID = fmt.Sprintf("%d", time.Now().UnixNano())
+		trade.Timestamp = time.Now().Format(time.RFC3339)
+
+		// Save Trade Action in DynamoDB
+		go saveTradeToDynamoDB(trade)
+
+		// Broadcast trade action to WebSocket clients
+		broadcastTrade(trade)
+	}
+}
+
+// Save Trade Action to DynamoDB
+func saveTradeToDynamoDB(trade TradeAction) {
+	_, err := dynamoDB.PutItem(&dynamodb.PutItemInput{
+		TableName: aws.String(dynamoDBTable), // Using Pulumi provisioned table
+		Item: map[string]*dynamodb.AttributeValue{
+			"TradeID":   {S: aws.String(trade.TradeID)},
+			"Timestamp": {S: aws.String(trade.Timestamp)},
+			"Price":     {N: aws.String(fmt.Sprintf("%.2f", trade.Price))},
+			"Volume":    {N: aws.String(fmt.Sprintf("%d", trade.Volume))},
+			"Trader":    {S: aws.String(trade.Trader)},
+			"Action":    {S: aws.String(trade.Action)},
+		},
+	})
+	if err != nil {
+		log.Println("Error saving trade to DynamoDB:", err)
+		return
+	}
+
+	// Publish Trade Action to SNS
+	publishTradeToSNS(trade)
+}
+
+// Publish Trade Action to SNS
+func publishTradeToSNS(trade TradeAction) {
+	data, _ := json.Marshal(trade)
+	_, err := snsClient.Publish(&sns.PublishInput{
+		TopicArn: aws.String(snsTopicArn), // Using Pulumi provisioned SNS topic
+		Message:  aws.String(string(data)),
+	})
+	if err != nil {
+		log.Println("Error publishing to SNS:", err)
+	}
+}
+
+// Broadcast Trade Action to WebSocket Clients
+func broadcastTrade(trade TradeAction) {
+	data, _ := json.Marshal(trade)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	for client := range clients {
+		err := client.WriteMessage(websocket.TextMessage, data)
+		if err != nil {
+			log.Println("WebSocket Write Error:", err)
+			client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
+// Start WebSocket Server
+func main() {
+	http.HandleFunc("/ws", handleConnections)
+
+	port := ":8080"
+	log.Println("WebSocket server running on port", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
